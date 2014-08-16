734401885ba4cc85860a35afca9b98b1dc89ff79
g2minhle <lehoangminh@live.com>
2014-08-16 03:42:56 -0400

    Add fancy homepage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('myApp.controllers', []).controller('MyCtrl1',
+		[ '$scope', function($scope) {
+			$(document).ready(function() {
+				$('#fullpage').fullpage({
+					sectionsColor: ['#42bd41', '#42bd41'],
+					navigation: true,
+					autoScrolling: true,
+					animateAnchor: true,
+				});
+			});
+		} ]).controller('MyCtrl2', [ '$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+} ]);
